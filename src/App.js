4762369ffbb5a80331559c3a82abb7e21730b12a
sshanbhag09 <69388704+sshanbhag09@users.noreply.github.com>
2022-08-29 19:55:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React,{useEffect, useState} from 'react'
-import alanBtn from '@alan-ai/alan-sdk-web'
+import React,{useEffect, useRef, useState} from 'react'
+import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from './NewsCards/NewsCards';
 import "./App.css"
 import { mergeClasses } from '@material-ui/styles';
-const  alanKey="NEWS API"
 
+const  alanKey="e63211bab92dd6c593505e56c9fe94662e956eca572e1d8b807a3e2338fdd0dc/stage"
+function useStateReference(value) {
+  const ref = useRef(value);
+  const [, forceRender] = useState(false);
+
+  function updateState(newState) {
+    if (!Object.is(ref.current, newState)) {
+      ref.current = newState;
+      forceRender(s => !s);
+    }
+  }
+
+  return [ref, updateState];
+}
 function App() {
+  const [count, setCount] = useStateReference({});
   const[newsArticles,setNewsArticles] =useState([]);
   useEffect(()=>{
 
